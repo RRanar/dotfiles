6e2cd7d0d00ff5efd3949fc0f0706812249d3717
RRanar <kombidemon@gmail.com>
2021-04-25 18:21:12 +0300

    Add themes from https://github.com/adi1090x/polybar-themes and change it

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,85 @@
-#!/bin/bash
-# Запуск Polybar со стандартным расположением конфигурационного файла в ~/.config/polybar/config
-ps aux | grep hideIt | grep -v grep | awk '{print $2}' | xargs kill
-killall -q polybar
+#!/usr/bin/env bash
 
-polybar example  > /dev/null 2>&1 &
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
 
-sleep 1
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
 
-hideIt.sh -N '^polybar-example' -p 3 -d top -H > /dev/null 2>&1 &
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Polybar загрузился..."
+	# Launch the bar
+	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
+		polybar -q top -c "$dir/$style/config.ini" &
+		polybar -q bottom -c "$dir/$style/config.ini" &
+	elif [[ "$style" == "pwidgets" ]]; then
+		bash "$dir"/pwidgets/launch.sh --main
+	else
+		polybar -q main -c "$dir/$style/config.ini" &
+
+		sleep 1 
+
+		hideIt.sh -N "^polybar-example" -p 3 -d top -H > /dev/null 2>&1 & 	
+	fi
+}
+
+if [[ "$1" == "--material" ]]; then
+	style="material"
+	launch_bar
+
+elif [[ "$1" == "--shades" ]]; then
+	style="shades"
+	launch_bar
+
+elif [[ "$1" == "--hack" ]]; then
+	style="hack"
+	launch_bar
+
+elif [[ "$1" == "--docky" ]]; then
+	style="docky"
+	launch_bar
+
+elif [[ "$1" == "--cuts" ]]; then
+	style="cuts"
+	launch_bar
+
+elif [[ "$1" == "--shapes" ]]; then
+	style="shapes"
+	launch_bar
+
+elif [[ "$1" == "--grayblocks" ]]; then
+	style="grayblocks"
+	launch_bar
+
+elif [[ "$1" == "--blocks" ]]; then
+	style="blocks"
+	launch_bar
+
+elif [[ "$1" == "--colorblocks" ]]; then
+	style="colorblocks"
+	launch_bar
+
+elif [[ "$1" == "--forest" ]]; then
+	style="forest"
+	launch_bar
+
+elif [[ "$1" == "--pwidgets" ]]; then
+	style="pwidgets"
+	launch_bar
+
+elif [[ "$1" == "--panels" ]]; then
+	style="panels"
+	launch_bar
+
+else
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	--blocks    --colorblocks    --cuts      --docky
+	--forest    --grayblocks     --hack      --material
+	--panels    --pwidgets       --shades    --shapes
+	EOF
+fi
